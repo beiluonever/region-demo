4cb0bf403aec6042f2c6fe666bbabc6890a98b1d
beiluonever <beiluo1994@gmail.com>
2023-11-24 14:03:01 +0800

    feat: change region tree build use recursion function

--- src/main/java/com/example/regiondemo/service/RegionService.java
+++ src/main/java/com/example/regiondemo/service/RegionService.java
@@ -19,71 +19,67 @@ public class RegionService {
     RegionMapper regionMapper;
 
     public List<RegionTreeVO> getRegionTree(String pCode, String type) {
-        if(StringUtils.isNullOrEmpty(type)){
-            //默认选区县村
-            type = "2";
-        }
-        boolean hasHefei = "2".equals(type);
 
         List<Region> regionList = regionMapper.getRegion(pCode, null);
         List<RegionTreeVO> result = new ArrayList<>();
-        List<RegionTreeVO> townResult = new ArrayList<>();
-        List<RegionTreeVO> vaillgeResult = new ArrayList<>();
-        if (CollectionUtils.isEmpty(regionList)) {
-            return result;
-        }
-
-        Map<String, List<Region>> pCodeMap = regionList.stream().collect(Collectors.groupingBy(Region::getRegionParentId));
-        Map<String, List<RegionTreeVO>> townPCodeMap = new HashMap<>();
-        List<RegionTreeVO> topLevel = new ArrayList<>();
-        String finalType = type;
-        String finalType1 = type;
-        regionList.forEach(region -> {
-            String code = region.getRegionId();
-            RegionTreeVO self = new RegionTreeVO(region.getRegionName(), region.getRegionId(),
-                    hasHefei ? region.getRegionLevel() + 1 : region.getRegionLevel(), region.getRegionParentId(), region.getRegionType());
-            if (region.getRegionLevel() == 1) {
-                List<Region> children = pCodeMap.get(code);
-
-                if (!CollectionUtils.isEmpty(children)) {
-                    List<RegionTreeVO> treeChild = new ArrayList<>();
-                    children.forEach(c ->{
-                                if (c.getRegionType().equals(finalType)) {
-                                    treeChild.add(new RegionTreeVO(c.getRegionName(), c.getRegionId(),
-                                            hasHefei ? c.getRegionLevel() +1 : c.getRegionLevel() , c.getRegionParentId(), c.getRegionType()));
-                                }
-                            }
-                    );
-                    self.setChildren(treeChild);
-                }
-                townResult.add(self);
-                List<RegionTreeVO> townList = townPCodeMap.getOrDefault(region.getRegionParentId(), new ArrayList<>());
-                townList.add(self);
-                townPCodeMap.put(region.getRegionParentId(), townList);
-            } else if (region.getRegionLevel() == 0) {
-                topLevel.add(self);
-            } else if (region.getRegionLevel() == 2) {
-                if (self.getType().equals(finalType1)) {
-                    vaillgeResult.add(self);
-                }
-            }
+        regionList.forEach(region->{
+            result.add(new RegionTreeVO(region.getRegionName(), region.getRegionId(),
+                   region.getRegionLevel(), region.getRegionParentId(), region.getRegionType()));
         });
-        topLevel.forEach(self -> {
-            self.setChildren(townPCodeMap.get(self.getCode()));
-            result.add(self);
-        });
-        if (!CollectionUtils.isEmpty(result)) {
-            if(hasHefei){
-                RegionTreeVO regionTop = new RegionTreeVO("合肥市", "0",
-                        0, "-1", "0");
-                regionTop.setChildren(result);
-                return Collections.singletonList(regionTop);
-            }
-            return result;
-        }
-        if (!CollectionUtils.isEmpty(townResult)) {
-            return townResult;
-        }
-        return vaillgeResult;
+        return buildTree(result);
     }
+
+
+
+    public List<RegionTreeVO> buildTree(List<RegionTreeVO> regions) {
+        List<RegionTreeVO> returnList = new ArrayList<>();
+        List<String> tempList = regions.stream().map(RegionTreeVO::getCode).toList();
+        for (RegionTreeVO region : regions) {
+            // 如果是顶级节点, 遍历该父节点的所有子节点
+            if (!tempList.contains(region.getpCode())) {
+                recursionFn(regions, region);
+                returnList.add(region);
+            }
+        }
+        if (returnList.isEmpty()) {
+            returnList = regions;
+        }
+        return returnList;
+    }
+
+    /**
+     * 递归列表
+     */
+    private void recursionFn(List<RegionTreeVO> list, RegionTreeVO t) {
+        // 得到子节点列表
+        List<RegionTreeVO> childList = getChildList(list, t);
+        t.setChildren(childList);
+        for (RegionTreeVO tChild : childList) {
+            if (hasChild(list, tChild)) {
+                recursionFn(list, tChild);
+            }
+        }
+    }
+
+
+    /**
+     * 得到子节点列表
+     */
+    private List<RegionTreeVO> getChildList(List<RegionTreeVO> list, RegionTreeVO t) {
+        List<RegionTreeVO> tlist = new ArrayList<>();
+        for (RegionTreeVO n : list) {
+            if (!StringUtils.isNullOrEmpty(n.getpCode()) && n.getpCode().equals(t.getCode())) {
+                tlist.add(n);
+            }
+        }
+        return tlist;
+    }
+
+    /**
+     * 判断是否有子节点
+     */
+    private boolean hasChild(List<RegionTreeVO> list, RegionTreeVO t) {
+        return getChildList(list, t).size() > 0;
+    }
+
 }
